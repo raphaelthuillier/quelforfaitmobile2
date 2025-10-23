97d8f5e5478f590dcb20371888755c05f77b0300
raphaelthuillier <raphael.thuillier@isoskele.fr>
2025-10-23 15:37:48 +0200

    Create config.js

---
+++ src/config.js
@@ -0,0 +1,2 @@
+export const API_URL = "https://sheet.tools.timeonegroup.com/api/v2/tables/mz5amo5hh2ax24v/records";
+export const API_TOKEN = "Bearer hQDY5bJcAtrsHPsr2mz4LQSmRxG0G7pqj-VXvsoq"; // ton token NocoDB
